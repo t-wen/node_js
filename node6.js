589181287fb28d8e39641717df401bdb5994bd2b
t-wen <30753500+t-wen@users.noreply.github.com>
2018-06-12 22:22:13 +0800

    Initial commit

---
+++ node6.js
@@ -0,0 +1,19 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+var listener1 = function listener1() {
+    console.log('监听器 listener1 执行。');
+}
+var listener2 = function listener2() {
+    console.log('监听器 listener2 执行。');
+}
+eventEmitter.addListener('connection', listener1);
+eventEmitter.on('connection', listener2);
+var eventListener2 = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " 个监听器监听连接事件。");
+eventEmitter.emit('connection');
+eventEmitter.removerListener('connection', listener1);
+console.log("listener1 不再受监听。");
+eventEmitter.emit('connection');
+eventListeners = require('events').EvenEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " 个监听器监听连接事件。");
+console.log("程序执行完毕。");
