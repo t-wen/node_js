b0783dbb71debffdb895826441ac3e7c4f0ae6dc
t-wen <30753500+t-wen@users.noreply.github.com>
2018-06-15 01:20:21 +0800

    update

---
+++ node45.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+console.log("准备删除目录 /tmp/test");
+fs.rmdir("/tmp/test",function(err){
+    if(err) {
+        return console.error(err);
+    }
+    console.log("读取 /tmp 目录");
+    fs.readdir("/tmp/",function(err, files){
+        if (err) {
+            return console.error(err);
+        }
+        files.forEach( function (file){
+            console.log( file );
+        });
+    });
+});
