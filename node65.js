27b30c646c6c158a1fc0a7ac10740752aafa6dba
t-wen <30753500+t-wen@users.noreply.github.com>
2018-06-17 01:48:45 +0800

    update

---
+++ node65.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+app.get('/:id', function (req, res) {
+    fs.readFile( __dirname + "/" + "node_users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        var user = data["user" + req.params.id]
+        console.log( user );
+        res.end( JSON.stringify(user) );
+    });
+})
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
